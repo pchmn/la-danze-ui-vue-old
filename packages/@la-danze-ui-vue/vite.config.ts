4626c6f7f5d08ece2fa2cb7e9cfaa784f6bca9b0
pchmn <pchmn.dev@gmail.com>
2021-06-01 11:04:40 +0200

    Init project

---
+++ packages/@la-danze-ui-vue/vite.config.ts
@@ -0,0 +1,26 @@
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'LaDanzeUi',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  }
+})
---
+++ packages/docs/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a
+      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+      target="_blank"
+    >
+      Vetur
+    </a>
+    or
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    (if using
+    <code>&lt;script setup&gt;</code>)
+  </p>
+
+  <p>See <code>README.md</code> for more information.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <button
+    class="bg-purple-600 hover:bg-purple-700 shadow focus:outline-none focus:ring focus:ring-purple-500 focus:ring-opacity-40 px-5 py-2 text-white rounded-lg font-semibold transition-all"
+  >
+    Buy now
+  </button>
+
+  <input
+    type="text"
+    class="py-2 px-3 border hover:border-gray-500 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-opacity-40 focus:border-purple-600 rounded-lg transition-shadow transition-colors"
+    placeholder="test"
+  />
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
